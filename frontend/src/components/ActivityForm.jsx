e8ec2632d35795e56bb5fd40487aaeabf2699235
torvaldxd <marulanda.sebastian@correounivalle.edu.co>
2026-02-22 00:13:59 -0500

    se realizo el  despliegue frontend con vercel

--- frontend/src/components/ActivityForm.jsx
+++ frontend/src/components/ActivityForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000/api';
 
 const ActivityForm = () => {
     const navigate = useNavigate();
